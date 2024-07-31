b34c68028b4345a72416f1f4b3445f41066d205d
KC1064 <kiron08072003@gmail.com>
2024-07-31 14:40:12 +0530

    Restarted the whole project

--- script.js
+++
@@ -1,7 +0,0 @@
-const text = document.getElementById("text");
-const input = document.getElementById("input-field");
-const timer = document.querySelector("#timer p span");
-const mistakes = document.querySelector("#mistakes p span");
-const wpm = document.querySelector('#wpm p span');
-const cpm = document.querySelector("#cpm p span");
-const btn = document.querySelector('button')
